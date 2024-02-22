79dac6fa19f8fbb676d2d6f90f2a3eb05a59f60b
Suresh-Venna <venkatapythondeveloper@gmail.com>
2024-02-22 17:07:33 -0600

    view changed

--- pages/views.py
+++ pages/views.py
@@ -5,4 +5,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_view(request):
-    return HttpResponse("Hello! World - Venkata Suresh Venna")
+    return HttpResponse("Hello! World Weolcome to world of Venkata Suresh Venna")
